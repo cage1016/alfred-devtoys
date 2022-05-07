385466f9ed1db36d3a29460b0e0ab08d527634be
Kai-Chu Chung <cage.chung@gmail.com>
2022-05-08 00:15:21 +0800

    fix release package invalid

--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@
 source env.sh
 GOARCH=amd64 GOOS=darwin go build -ldflags "-s -w" -o ".workflow/exe" .
 /usr/local/bin/envsubst >.workflow/info.plist <./info.plist.template
-zip -r -D ./${alfred_workflow_name}-${alfred_workflow_version}.alfredworkflow .workflow/*
+cd .workflow && zip -r -D ../${alfred_workflow_name}-${alfred_workflow_version}.alfredworkflow ./*
 
 echo "release ./${alfred_workflow_name}-${alfred_workflow_version}.alfredworkflow success"
