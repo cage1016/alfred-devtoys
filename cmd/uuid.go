19faa430b6c8a3f7cb86771720e9b96f1fbac93d
Kai-Chu Chung <cage.chung@gmail.com>
2022-07-12 23:32:53 +0800

    add uuid

---
+++ cmd/uuid.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	aw "github.com/deanishe/awgo"
+	"github.com/google/uuid"
+	"github.com/spf13/cobra"
+)
+
+// uuidCmd represents the uuid command
+var uuidCmd = &cobra.Command{
+	Use:   "uuid",
+	Short: "uuid string",
+	Run:   runUuid,
+}
+
+func runUuid(cmd *cobra.Command, args []string) {
+	if len(args) >= 1 {
+		query := strings.Join(args, " ")
+		c, err := strconv.ParseInt(query, 10, 64)
+		if err != nil {
+			wf.NewItem(err.Error()).Subtitle("Invalid integer").Valid(false).Icon(aw.IconError)
+		}
+
+		buf := make([]string, c)
+		for i := int64(0); i < c; i++ {
+			buf[i] = uuid.New().String()
+		}
+		for _, v := range buf {
+			wf.NewItem(v).Subtitle("UUID, ⌥ to copy all").Valid(true).Arg(v).Icon(&aw.Icon{Value: "uuid.pdf"}).Var("action", "copy").Valid(true).
+				Opt().Subtitle(fmt.Sprintf("UUID, copy all of %d", c)).Arg(strings.Join(buf, "\n")).Var("action", "copy")
+		}
+	}
+
+	wf.WarnEmpty("No matching items", "Try how many UUID you want to generate")
+	wf.SendFeedback()
+}
+
+func init() {
+	rootCmd.AddCommand(uuidCmd)
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/deanishe/awgo v0.29.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-xmlfmt/xmlfmt v0.0.0-20220206211657-0a94163c4677 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/ku/go-change-case v0.0.1 // indirect
 	github.com/spf13/cobra v1.5.0
 	golang.design/x/clipboard v0.6.2 // indirect
